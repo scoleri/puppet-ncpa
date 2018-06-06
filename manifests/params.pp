7597a7932fa937a13ea6438e6a3b7de8f5c4b0e7
Scoleri, Steven <scoleris@radial.com>
2018-06-06 15:17:24 -0400

    Updated the URL for the repo

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@
 class ncpa::params {
   case $::os['family'] {
     'RedHat': {
-      $rpmrepo_url = 'https://repo.nagios.com/nagios/7/nagios-repo-7-2.el7.noarch.rpm'
+      $rpmrepo_url = 'https://repo.nagios.com/nagios/7/nagios-repo-7-3.el7.noarch.rpm'
     }
     default: {
       fail("Sorry, ${::os['family']} is not supported.")
