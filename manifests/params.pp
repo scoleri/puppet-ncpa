17e5e71d0f7c9160bb78371b92970459bcd76175
Scoleri, Steven <scoleris@radial.com>
2018-06-06 15:57:24 -0400

    added all the redhat majors

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,18 @@
 # == Class: ncpa::params
 #
 class ncpa::params {
-  case $::os['family'] {
+  casej$::os['family'] {
     'RedHat': {
-      $rpmrepo_url = 'https://repo.nagios.com/nagios/7/nagios-repo-7-3.el7.noarch.rpm'
-    }
+      casej$::os['release']['major']{
+        '7': {
+          $rpmrepo_url = 'https://repo.nagios.com/nagios/7/nagios-repo-7-3.el7.noarch.rpm'
+        }
+        '6': {
+          $rpmrepo_url = 'https://repo.nagios.com/nagios/6/nagios-repo-6-3.el6.noarch.rpm'
+         }
+        '5': {
+          $rpmrepo_url = 'https://repo.nagios.com/nagios/5/nagios-repo-5-2.el5.noarch.rpm'
+        }
     default: {
       fail("Sorry, ${::os['family']} is not supported.")
     }
